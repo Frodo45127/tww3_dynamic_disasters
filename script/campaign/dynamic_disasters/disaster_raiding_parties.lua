7500d9a26e1e9f20bbcfe3e094e9912c679e6ff2
Ismael Gutiérrez González <frodo_gv@hotmail.com>
2022-09-11 05:39:36 +0200

    Improved raiding parties logic.

--- script/campaign/dynamic_disasters/disaster_raiding_parties.lua
+++ script/campaign/dynamic_disasters/disaster_raiding_parties.lua
@@ -48,6 +48,8 @@ disaster_raiding_parties = {
         repeteable = true,                  -- If the disaster can be repeated.
         is_endgame = false,                 -- If the disaster is an endgame.
         min_turn = 30,                      -- Minimum turn required for the disaster to trigger.
+        max_turn = 0,                       -- If the disaster hasn't trigger at this turn, we try to trigger it. Set to 0 to not check for max turn. Used only for some disasters.
+        status = 0,                         -- Current status of the disaster. Used to re-initialize the disaster correctly on reload.
         last_triggered_turn = 0,            -- Turn when the disaster was last triggerd.
         last_finished_turn = 0,             -- Turn when the disaster was last finished.
         wait_turns_between_repeats = 10,    -- If repeteable, how many turns will need to pass after finished for the disaster to be available again.
@@ -57,15 +59,15 @@ disaster_raiding_parties = {
         },
 
         -- Disaster-specific data.
-        warning_delay = 1,
         army_template = {},
-        base_army_unit_count = 19,
+        unit_count = 19,
+        warning_delay = 1,
 
         faction = "",
         subculture = "",
-        potential_attack_factions_alive = {},
-        potential_attack_subcultures_alive = {},
-        potential_attack_factions = {         -- Factions that will receive the attacking armies.
+        factions_alive = {},
+        subcultures_alive = {},
+        factions = {           -- Factions that will receive the attacking armies.
             wh2_dlc11_sc_cst_vampire_coast = {      -- Vampire Coast
                 "wh2_dlc11_cst_vampire_coast",          -- Harkon
                 "wh2_dlc11_cst_noctilus",               -- Noctilus
@@ -88,8 +90,8 @@ disaster_raiding_parties = {
     },
     warning_event_key = "fro_dyn_dis_raiding_parties_warning",
     raiding_event_key = "fro_dyn_dis_raiding_parties_trigger",
-    raiding_cancel_event_key = "fro_dyn_dis_raiding_parties_early_end",
-    raiding_raiders_effect_key = "fro_dyn_dis_raiding_parties_invader_buffs",
+    invader_buffs_effects_key = "fro_dyn_dis_raiding_parties_invader_buffs",
+    finish_early_incident_key = "fro_dyn_dis_raiding_parties_early_end",
 }
 
 
@@ -190,14 +192,14 @@ local potential_coasts = {
         "wh3_main_combi_region_straits_of_lothern",
     },
 
-    -- Who the fuck would raid that?
-    antartid = {
-        "wh3_main_combi_region_the_daemonium_coast",
-        "wh3_main_combi_region_serpent_coast_sea",
-        "wh3_main_combi_region_the_churning_gulf",
-        "wh3_main_combi_region_daemons_landing",
-        "wh3_main_combi_region_the_lustria_straight",
-    },
+    -- Who the fuck would raid that? The AI, aparently. A lot.
+    --antartid = {
+    --    "wh3_main_combi_region_the_daemonium_coast",
+    --    "wh3_main_combi_region_serpent_coast_sea",
+    --    "wh3_main_combi_region_the_churning_gulf",
+    --    "wh3_main_combi_region_daemons_landing",
+    --    "wh3_main_combi_region_the_lustria_straight",
+    --},
 }
 
 -- Function to set the status of the disaster, initializing the needed listeners in the process.
@@ -214,7 +216,11 @@ function disaster_raiding_parties:set_status(status)
                 return cm:turn_number() == self.settings.last_triggered_turn + self.settings.warning_delay
             end,
             function()
-                self:trigger_raiding_parties();
+                if self:update_alive() == false then
+                    dynamic_disasters:execute_payload(self.finish_early_incident_key, nil, 0, nil);
+                else
+                    self:trigger_raiding_parties();
+                end
                 self:trigger_end_disaster();
                 core:remove_listener("RaidingPartiesWarning")
             end,
@@ -227,9 +233,26 @@ end
 function disaster_raiding_parties:trigger()
     out("Frodo45127: Starting disaster: " .. self.name);
 
+    -- Recalculate the delay for further executions.
+    if dynamic_disasters.settings.debug == false then
+        self.settings.warning_delay = math.random(4, 10);
+        self.settings.wait_turns_between_repeats = self.settings.warning_delay + 10;
+    else
+        self.settings.warning_delay = 1;
+        self.settings.wait_turns_between_repeats = 1;
+    end
+
+    dynamic_disasters:execute_payload(self.warning_event_key, nil, 0, nil);
+    self:set_status(STATUS_TRIGGERED);
+end
+
+-- Function to trigger the raid itself.
+function disaster_raiding_parties:trigger_raiding_parties()
+    out("Frodo45127: Disaster: " .. self.name .. ". Triggering invasion.");
+
     -- Get the subculture/faction to use as raiders.
-    self.settings.subculture = self.settings.potential_attack_subcultures_alive[math.random(1, #self.settings.potential_attack_subcultures_alive)];
-    self.settings.faction = self.settings.potential_attack_factions_alive[self.settings.subculture][math.random(1, #self.settings.potential_attack_factions_alive[self.settings.subculture])];
+    self.settings.subculture = self.settings.subcultures_alive[math.random(1, #self.settings.subcultures_alive)];
+    self.settings.faction = self.settings.factions_alive[self.settings.subculture][math.random(1, #self.settings.factions_alive[self.settings.subculture])];
 
     -- Get the army template to use, based on the subculture and turn.
     local current_turn = cm:turn_number();
@@ -258,23 +281,6 @@ function disaster_raiding_parties:trigger()
         self.settings.army_template.dark_elves = template;
     end
 
-    -- Recalculate the delay for further executions.
-    if dynamic_disasters.settings.debug == false then
-        self.settings.warning_delay = math.random(4, 10);
-        self.settings.wait_turns_between_repeats = self.settings.warning_delay + 10;
-    else
-        self.settings.warning_delay = 1;
-        self.settings.wait_turns_between_repeats = 1;
-    end
-
-    self:set_status(STATUS_TRIGGERED);
-    dynamic_disasters:execute_payload(self.warning_event_key, nil, 0, nil);
-end
-
--- Function to trigger the raid itself.
-function disaster_raiding_parties:trigger_raiding_parties()
-    out("Frodo45127: Disaster: " .. self.name .. ". Triggering invasion.");
-
     -- Get all the coastal regions (as in region with a port) to attack by weight.
     local attack_vectors = {};
     local avg = 0;
@@ -290,15 +296,17 @@ function disaster_raiding_parties:trigger_raiding_parties()
 
             for _, land_region in pairs(sea_region_data.coastal_regions) do
                 local region = cm:get_region(land_region)
-                local region_owner = region:owning_faction()
+                if not region == false and region:is_null_interface() == false then
+                    local region_owner = region:owning_faction()
 
-                -- Do not attack your own subculture. Attack everyone else.
-                if region_owner:is_null_interface() == false and region_owner:subculture() ~= self.settings.subculture and region_owner:name() ~= "rebels" then
-                    attack_vectors[coast] = attack_vectors[coast] + 1;
+                    -- Do not attack your own subculture. Attack everyone else.
+                    if not region_owner == false and region_owner:is_null_interface() == false and region_owner:subculture() ~= self.settings.subculture then
+                        attack_vectors[coast] = attack_vectors[coast] + 1;
 
-                    -- Give a bit more priority to player coastal settlements.
-                    if region_owner:is_human() == true then
-                        attack_vectors[coast] = attack_vectors[coast] + 0.5;
+                        -- Give a bit more priority to player coastal settlements.
+                        if region_owner:is_human() == true then
+                            attack_vectors[coast] = attack_vectors[coast] + 0.5;
+                        end
                     end
                 end
             end
@@ -318,11 +326,11 @@ function disaster_raiding_parties:trigger_raiding_parties()
         end
     end
 
-    -- If no coast to attack has been found, or we wiped out the entire faction, just cancel the attack.
+    -- If no coast to attack has been found, just cancel the attack.
     local faction = cm:get_faction(self.settings.faction);
-    if #coasts_to_attack < 1 or (faction:is_null_interface() == true or faction:is_dead() or faction:was_confederated()) then
+    if #coasts_to_attack < 1 then
+        dynamic_disasters:execute_payload(self.finish_early_incident_key, nil, 0, nil);
         cm:activate_music_trigger("ScriptedEvent_Negative", self.settings.subculture)
-        dynamic_disasters:execute_payload(self.raiding_cancel_event_key, nil, 0, nil);
         return
     end
 
@@ -347,25 +355,21 @@ function disaster_raiding_parties:trigger_raiding_parties()
         -- Spawn armies at sea.
         for i = 1, #dyn_dis_sea_potential_attack_vectors[sea_region].spawn_positions do
             local spawn_pos = dyn_dis_sea_potential_attack_vectors[sea_region].spawn_positions[i];
-            dynamic_disasters:create_scenario_force_at_coords(self.settings.faction, dyn_dis_sea_potential_attack_vectors[sea_region].coastal_regions[1], spawn_pos, self.settings.army_template, self.settings.base_army_unit_count, true, armies_to_spawn_in_each_spawn_point, self.name);
+            dynamic_disasters:create_scenario_force_at_coords(self.settings.faction, dyn_dis_sea_potential_attack_vectors[sea_region].coastal_regions[1], spawn_pos, self.settings.army_template, self.settings.unit_count, false, armies_to_spawn_in_each_spawn_point, self.name);
         end
     end
 
-    -- Trigger all the stuff related to the invasion (missions, effects,...).
-    cm:activate_music_trigger("ScriptedEvent_Negative", self.settings.subculture)
-    dynamic_disasters:execute_payload(self.raiding_event_key, nil, 0, dyn_dis_sea_potential_attack_vectors[first_sea_region].coastal_regions[1]);
-
-    -- Set diplomacy.
-    if faction:is_null_interface() == false and not faction:is_dead() then
-
-        -- Apply buffs to the attackers, so they can at least push one province into player territory.
-        cm:apply_effect_bundle(self.raiding_raiders_effect_key, self.settings.faction, 10)
-
-        -- Make every attacking faction go full retard against the owner of the coastal provinces.
+    -- Make every attacking faction go full retard against the owner of the coastal provinces.
+    if not faction == false and faction:is_null_interface() == false and not faction:is_dead() then
         for _, sea_region in pairs(potential_coasts[coast_to_attack]) do
             dynamic_disasters:declare_war_for_owners_and_neightbours(faction, dyn_dis_sea_potential_attack_vectors[sea_region].coastal_regions, false, {self.settings.subculture});
         end
     end
+
+    -- Trigger all the stuff related to the invasion (missions, effects,...).
+    cm:apply_effect_bundle(self.invader_buffs_effects_key, self.settings.faction, 10)
+    dynamic_disasters:execute_payload(self.raiding_event_key, nil, 0, dyn_dis_sea_potential_attack_vectors[first_sea_region].coastal_regions[1]);
+    cm:activate_music_trigger("ScriptedEvent_Negative", self.settings.subculture)
 end
 
 -- Function to trigger cleanup stuff after the invasion is over.
@@ -380,6 +384,41 @@ end
 ---@return boolean If the disaster will be triggered or not.
 function disaster_raiding_parties:check_start_disaster_conditions()
 
+    -- If none of the available factions is alive, do not trigger the disaster.
+    if self:update_alive() == false then
+        return false;
+    end
+
+    -- Debug mode support.
+    if dynamic_disasters.settings.debug == true then
+        return true;
+    end
+
+    -- Base chance: 1/50 turns (2%).
+    local base_chance = 0.02;
+
+    -- Increase the change of starting based on how many attackers are already dead.
+    -- In theory, no need to remove again confederated factions.
+    for _, factions in pairs(self.settings.factions) do
+        for _, faction_key in pairs(factions) do
+            local faction = cm:get_faction(faction_key);
+            if not faction == false and faction:is_null_interface() == false and faction:is_dead() then
+                base_chance = base_chance + 0.01;
+            end
+        end
+    end
+
+    if math.random() < base_chance then
+        return true;
+    end
+
+    return false;
+end
+
+-- Function to update the list of alive faction/subculture. Moved here because the logic got complex and we need to reuse it.
+---@return boolean #If we got at least one faction/subculture alive.
+function disaster_raiding_parties:update_alive()
+
     -- First, get all player subcultures. We need to discard them from the "alive" list.
     local human_factions = cm:get_human_factions()
     local human_subcultures = {}
@@ -390,10 +429,10 @@ function disaster_raiding_parties:check_start_disaster_conditions()
     -- Second, only trigger it if any of the factions is alive.
     -- We not only use this to check if a faction is alive, but also to get what factions/subcultures are still alive.
     -- That way we can only pickup factions that are actually alive, and end the raidings if you wipe them out.
-    self.settings.potential_attack_factions_alive = {};
-    self.settings.potential_attack_subcultures_alive = {};
+    self.settings.factions_alive = {};
+    self.settings.subcultures_alive = {};
     local count_alive = 0;
-    for subculture, factions in pairs(self.settings.potential_attack_factions) do
+    for subculture, factions in pairs(self.settings.factions) do
 
         -- Update the potential factions removing the confederated ones.
         factions = dynamic_disasters:remove_confederated_factions_from_list(factions);
@@ -412,7 +451,7 @@ function disaster_raiding_parties:check_start_disaster_conditions()
             local count = 0;
             for _, faction_key in pairs(factions) do
                 local faction = cm:get_faction(faction_key, true);
-                if faction:is_null_interface() == false and not faction:is_dead() then
+                if not faction == false and faction:is_null_interface() == false and not faction:is_dead() then
                     count = count + 1;
                     table.insert(factions_alive, faction_key);
                 end
@@ -420,44 +459,14 @@ function disaster_raiding_parties:check_start_disaster_conditions()
 
             if count > 0 then
                 count_alive = count_alive + 1;
-                self.settings.potential_attack_factions_alive[subculture] = factions_alive;
-                table.insert(self.settings.potential_attack_subcultures_alive, subculture);
+                self.settings.factions_alive[subculture] = factions_alive;
+                table.insert(self.settings.subcultures_alive, subculture);
             end
         end
     end
 
     -- If none of the available factions is alive, do not trigger the disaster.
-    if count_alive == 0 then
-        return false;
-    end
-
-    -- Debug mode support.
-    if dynamic_disasters.settings.debug == true then
-        return true;
-    end
-
-    -- Base chance: 1/50 turns (2%).
-    local base_chance = 0.02;
-
-    -- Increase the change of starting based on how many attackers are already dead.
-    -- In theory, no need to remove again confederated factions.
-    for _, factions in pairs(self.settings.potential_attack_factions) do
-        for _, faction_key in pairs(factions) do
-            local faction = cm:get_faction(faction_key);
-            if faction:is_null_interface() == false and faction:is_dead() then
-
-                -- Increase in 1% the chance of triggering for each dead attacker.
-                base_chance = base_chance + 0.01;
-            end
-        end
-    end
-
-    if math.random() < base_chance then
-        return true;
-    end
-
-    return false;
+    return count_alive >= 0
 end
-
 
 return disaster_raiding_parties
