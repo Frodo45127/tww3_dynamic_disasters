10c398c4b1dee7371cbab5923b48123057925d0c
Frodo45127 <frodo_gv@hotmail.com>
2022-09-22 16:27:39 +0200

    Updated integrations.

--- script/campaign/dynamic_disasters_integrations/expanded_roster_greenskins_goblin_pack.lua
+++ script/campaign/dynamic_disasters_integrations/expanded_roster_greenskins_goblin_pack.lua
@@ -11,13 +11,13 @@ if dynamic_disasters then
     local error_message = false
 
     -- Infantry
-    error_message = dynamic_disasters:add_unit_to_army_template("greenskins", "lategame", "grn_inf_forest_bow", 2); if is_string(error_message) then out("\t\t" .. error_message); end
-    error_message = dynamic_disasters:add_unit_to_army_template("greenskins", "lategame", "grn_inf_night_goblin_spears", 2); if is_string(error_message) then out("\t\t" .. error_message); end
+    error_message = dynamic_disasters:add_unit_to_army_template("greenskins", "lategame_goblins", "grn_inf_forest_bow", 2); if is_string(error_message) then out("\t\t" .. error_message); end
+    error_message = dynamic_disasters:add_unit_to_army_template("greenskins", "lategame_goblins", "grn_inf_night_goblin_spears", 2); if is_string(error_message) then out("\t\t" .. error_message); end
 
     -- Monsters
-    error_message = dynamic_disasters:add_unit_to_army_template("greenskins", "lategame", "colossal_squig", 2); if is_string(error_message) then out("\t\t" .. error_message); end
-    error_message = dynamic_disasters:add_unit_to_army_template("greenskins", "lategame", "armored_colossal_squig", 2); if is_string(error_message) then out("\t\t" .. error_message); end
+    error_message = dynamic_disasters:add_unit_to_army_template("greenskins", "lategame_goblins", "colossal_squig", 2); if is_string(error_message) then out("\t\t" .. error_message); end
+    error_message = dynamic_disasters:add_unit_to_army_template("greenskins", "lategame_goblins", "armored_colossal_squig", 2); if is_string(error_message) then out("\t\t" .. error_message); end
 
     -- Heroes
-    error_message = dynamic_disasters:add_unit_to_army_template("greenskins", "lategame", "wh_grn_forest_goblin_warboss", 1); if is_string(error_message) then out("\t\t" .. error_message); end
+    error_message = dynamic_disasters:add_unit_to_army_template("greenskins", "lategame_goblins", "wh_grn_forest_goblin_warboss", 1); if is_string(error_message) then out("\t\t" .. error_message); end
 end
--- script/campaign/dynamic_disasters_integrations/expanded_roster_greenskins_orc_pack.lua
+++ script/campaign/dynamic_disasters_integrations/expanded_roster_greenskins_orc_pack.lua
@@ -10,17 +10,31 @@ out("\tFrodo45127: Trying to add units from integration (Expanded Roster - Green
 if dynamic_disasters then
     local error_message = false
 
+    ----------------------------------------
+    -- Lategame Orcs
+    ----------------------------------------
     -- Infantry
-    error_message = dynamic_disasters:add_unit_to_army_template("greenskins", "lategame", "grn_inf_black_orc_shields", 6); if is_string(error_message) then out("\t\t" .. error_message); end
-    error_message = dynamic_disasters:add_unit_to_army_template("greenskins", "lategame", "grn_inf_black_orc_dual", 6); if is_string(error_message) then out("\t\t" .. error_message); end
-    error_message = dynamic_disasters:add_unit_to_army_template("greenskins", "lategame", "grn_big_uns_shields", 4); if is_string(error_message) then out("\t\t" .. error_message); end
-    error_message = dynamic_disasters:add_unit_to_army_template("greenskins", "lategame", "grn_inf_savage_big_great", 4); if is_string(error_message) then out("\t\t" .. error_message); end
-    error_message = dynamic_disasters:add_unit_to_army_template("greenskins", "lategame", "grn_mon_savage_giant", 2); if is_string(error_message) then out("\t\t" .. error_message); end
+    error_message = dynamic_disasters:add_unit_to_army_template("greenskins", "lategame_orcs", "grn_inf_black_orc_shields", 6); if is_string(error_message) then out("\t\t" .. error_message); end
+    error_message = dynamic_disasters:add_unit_to_army_template("greenskins", "lategame_orcs", "grn_inf_black_orc_dual", 6); if is_string(error_message) then out("\t\t" .. error_message); end
+    error_message = dynamic_disasters:add_unit_to_army_template("greenskins", "lategame_orcs", "grn_big_uns_shields", 4); if is_string(error_message) then out("\t\t" .. error_message); end
+    error_message = dynamic_disasters:add_unit_to_army_template("greenskins", "lategame_orcs", "grn_inf_savage_big_great", 2); if is_string(error_message) then out("\t\t" .. error_message); end
 
     -- Monsters
-    error_message = dynamic_disasters:add_unit_to_army_template("greenskins", "lategame", "grn_mon_savage_giant", 2); if is_string(error_message) then out("\t\t" .. error_message); end
+    error_message = dynamic_disasters:add_unit_to_army_template("greenskins", "lategame_orcs", "grn_mon_savage_giant", 2); if is_string(error_message) then out("\t\t" .. error_message); end
 
     -- Heroes
-    error_message = dynamic_disasters:add_unit_to_army_template("greenskins", "lategame", "wh_grn_orc_boss", 1); if is_string(error_message) then out("\t\t" .. error_message); end
-    error_message = dynamic_disasters:add_unit_to_army_template("greenskins", "lategame", "wh_grn_savage_boss", 1); if is_string(error_message) then out("\t\t" .. error_message); end
+    error_message = dynamic_disasters:add_unit_to_army_template("greenskins", "lategame_orcs", "wh_grn_orc_boss", 1); if is_string(error_message) then out("\t\t" .. error_message); end
+
+    ----------------------------------------
+    -- Lategame Savage Orcs
+    ----------------------------------------
+    -- Infantry
+    error_message = dynamic_disasters:add_unit_to_army_template("greenskins", "lategame_savage_orcs", "grn_inf_savage_big_great", 4); if is_string(error_message) then out("\t\t" .. error_message); end
+
+    -- Monsters
+    error_message = dynamic_disasters:add_unit_to_army_template("greenskins", "lategame_savage_orcs", "grn_mon_savage_giant", 2); if is_string(error_message) then out("\t\t" .. error_message); end
+
+    -- Heroes
+    error_message = dynamic_disasters:add_unit_to_army_template("greenskins", "lategame_orcs", "wh_grn_savage_boss", 1); if is_string(error_message) then out("\t\t" .. error_message); end
+
 end
