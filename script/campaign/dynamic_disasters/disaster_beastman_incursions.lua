bcecff953dde72c179dc3a2953fc566f2e43d77a
Frodo45127 <frodo_gv@hotmail.com>
2022-11-21 22:13:55 +0100

    Added more ideas for future disasters.

---
+++ script/campaign/dynamic_disasters/disaster_beastman_incursions.lua
@@ -0,0 +1,7 @@
+--[[
+    Beastmen Incursions, By Frodo45127.
+
+    IDEA: Mid-late game repeatable disaster where beastmen start spawning in great numbers on certain beastmen-related regions.
+    Kinda like the skaven incursions, but adapted to beastmen. And increased probability if the chaos invasion has been triggered.
+
+]]
--- script/campaign/dynamic_disasters/disaster_three_kingdoms_2.lua
+++ script/campaign/dynamic_disasters/disaster_three_kingdoms_2.lua
@@ -1,1 +1,9 @@
-disaster_
+--[[
+    Three Kingdoms 2: Electric Boogaloo disaster, By Frodo45127.
+
+    IDEA: Similar to Three Kingdoms endgame:
+    - Find the 3 stronger faction.
+    - Make them consolidate their race.
+    - Force an all-out war amonst themselfs.
+
+]]
