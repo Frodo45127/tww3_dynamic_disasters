9a8e16ad45ef4ec572132ba65798ecedb06e9565
Frodo45127 <frodo_gv@hotmail.com>
2023-01-10 04:37:16 +0100

    Added mod integration for Expanded Roster - Norsca.

---
+++ script/campaign/dynamic_disasters_integrations/expanded_roster_norsca.lua
@@ -0,0 +1,88 @@
+--[[
+    Expanded Roster - Norsca: https://steamcommunity.com/sharedfiles/filedetails/?id=2907218501
+
+    Last Updated: 08/01/2023
+]]
+
+-- We initialize the integration on first tick, just after the main dynamic_disasters object has been initialized
+out("\tFrodo45127: Trying to add units from integration (Expanded Roster - Norsca).");
+
+if dynamic_disasters then
+    local error_message = false
+
+    ----------------------------------------
+    -- Norsca templates
+    ----------------------------------------
+    ---- Early Game
+    ----------------------------------------
+
+    -- Melee Infantry
+    error_message = dynamic_disasters:add_unit_to_army_template("norsca", "earlygame", "deco_nor_pit", 2); if is_string(error_message) then out("\t\t" .. error_message); end
+    error_message = dynamic_disasters:add_unit_to_army_template("norsca", "earlygame", "deco_nor_maidens", 3); if is_string(error_message) then out("\t\t" .. error_message); end
+
+    -- Ranged Infantry
+    error_message = dynamic_disasters:add_unit_to_army_template("norsca", "earlygame", "deco_nor_bondsmen", 4); if is_string(error_message) then out("\t\t" .. error_message); end
+    error_message = dynamic_disasters:add_unit_to_army_template("norsca", "earlygame", "deco_nor_reavers", 4); if is_string(error_message) then out("\t\t" .. error_message); end
+
+    -- Monsters
+    error_message = dynamic_disasters:add_unit_to_army_template("norsca", "earlygame", "deco_nor_valkyrie", 2); if is_string(error_message) then out("\t\t" .. error_message); end
+
+    -- Heroes
+    error_message = dynamic_disasters:add_unit_to_army_template("norsca", "earlygame", "deco_nor_jarl", 1); if is_string(error_message) then out("\t\t" .. error_message); end
+
+    ----------------------------------------
+    ---- Mid Game
+    ----------------------------------------
+
+    -- Melee Infantry
+    error_message = dynamic_disasters:add_unit_to_army_template("norsca", "earlygame", "deco_nor_pit", 4); if is_string(error_message) then out("\t\t" .. error_message); end
+    error_message = dynamic_disasters:add_unit_to_army_template("norsca", "earlygame", "deco_nor_maidens", 4); if is_string(error_message) then out("\t\t" .. error_message); end
+    error_message = dynamic_disasters:add_unit_to_army_template("norsca", "earlygame", "deco_nor_huskarls", 2); if is_string(error_message) then out("\t\t" .. error_message); end
+
+    -- Ranged Infantry
+    error_message = dynamic_disasters:add_unit_to_army_template("norsca", "earlygame", "deco_nor_reavers", 4); if is_string(error_message) then out("\t\t" .. error_message); end
+
+    -- Monsters
+    error_message = dynamic_disasters:add_unit_to_army_template("norsca", "earlygame", "deco_nor_valkyrie", 2); if is_string(error_message) then out("\t\t" .. error_message); end
+    error_message = dynamic_disasters:add_unit_to_army_template("norsca", "earlygame", "deco_nor_hydra", 1); if is_string(error_message) then out("\t\t" .. error_message); end
+
+    -- Heroes
+    error_message = dynamic_disasters:add_unit_to_army_template("norsca", "earlygame", "deco_nor_jarl", 1); if is_string(error_message) then out("\t\t" .. error_message); end
+
+
+    ----------------------------------------
+    ---- Late Game
+    ----------------------------------------
+
+    -- Melee Infantry
+    error_message = dynamic_disasters:add_unit_to_army_template("norsca", "earlygame", "deco_nor_pit", 2); if is_string(error_message) then out("\t\t" .. error_message); end
+    error_message = dynamic_disasters:add_unit_to_army_template("norsca", "earlygame", "deco_nor_maidens", 4); if is_string(error_message) then out("\t\t" .. error_message); end
+    error_message = dynamic_disasters:add_unit_to_army_template("norsca", "earlygame", "deco_nor_huskarls", 6); if is_string(error_message) then out("\t\t" .. error_message); end
+
+    -- Ranged Infantry
+    error_message = dynamic_disasters:add_unit_to_army_template("norsca", "earlygame", "deco_nor_reavers", 4); if is_string(error_message) then out("\t\t" .. error_message); end
+
+    -- Monsters
+    error_message = dynamic_disasters:add_unit_to_army_template("norsca", "earlygame", "deco_nor_valkyrie", 3); if is_string(error_message) then out("\t\t" .. error_message); end
+    error_message = dynamic_disasters:add_unit_to_army_template("norsca", "earlygame", "deco_nor_hydra", 1); if is_string(error_message) then out("\t\t" .. error_message); end
+
+    -- Heroes
+    error_message = dynamic_disasters:add_unit_to_army_template("norsca", "earlygame", "deco_nor_jarl", 1); if is_string(error_message) then out("\t\t" .. error_message); end
+
+    --[[
+    -- TODO: Integrate them into the chaos templates.
+    ----------------------------------------
+    -- Chaos templates
+    ----------------------------------------
+    -- Infantry
+    error_message = dynamic_disasters:add_unit_to_army_template("savage_orcs", "lategame", "grn_inf_savage_big_great", 4); if is_string(error_message) then out("\t\t" .. error_message); end
+
+    -- Monsters
+    error_message = dynamic_disasters:add_unit_to_army_template("savage_orcs", "lategame", "grn_mon_savage_giant", 2); if is_string(error_message) then out("\t\t" .. error_message); end
+
+    -- Heroes
+    error_message = dynamic_disasters:add_unit_to_army_template("savage_orcs", "lategame", "wh_grn_savage_boss", 1); if is_string(error_message) then out("\t\t" .. error_message); end
+    ]]
+
+end
+
